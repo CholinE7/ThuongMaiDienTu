9d53a72c7f64988628c3abab5cb842bdbf8e1088
thanhhieu3004 <153196711+thanhhieu3004@users.noreply.github.com>
2026-04-04 01:58:49 +0700

    ok
    ok

--- backend/target/generated-sources/annotations/com/tmdtud/cuahang/api/purchase_orders_detail/mapper/PurchaseOrderDetailMapperImpl.java
+++ backend/target/generated-sources/annotations/com/tmdtud/cuahang/api/purchase_orders_detail/mapper/PurchaseOrderDetailMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-04-04T01:46:49+0700",
+    date = "2026-04-04T01:51:00+0700",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260224-0835, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
